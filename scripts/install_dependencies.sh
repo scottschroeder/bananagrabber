7c038e9e3998bb6e9305f90b09152dbe50c4bb83
Scott Schroeder <scottschroeder@sent.com>
2022-05-22 22:07:30 -0400

    debugging help in install script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,5 +4,15 @@ sudo amazon-linux-extras install docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 
+echo "we are in the install script"
+
+echo "cwd:"
+pwd
+
+echo "here is the directory"
+ls -hla
+
+echo "and all the contents"
+cat ./*
 # aws ecr get-login-password --region "$(cat ECR_REGION)" | docker login --username AWS --password-stdin "$(cat ECR_REGISTRY)"
 docker login -u AWS -p $(aws ecr get-login-password --region $(cat ECR_REGION)) $(cat ECR_REGISTRY)
