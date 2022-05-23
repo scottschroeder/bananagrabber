a5b5dad4b2cc9ba8d57ac1636a566c5fe8f31721
Scott Schroeder <scottschroeder@sent.com>
2022-05-22 20:45:14 -0400

    app should login

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@
 echo "starting version $(cat version.txt)"
 # target is defined by the buildspec.yml and should contain the URI
 # of the built image
-docker run --rm "$(cat target)"
+docker run --rm "$(cat ECR_REGISTRY):$(cat target)"
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if ! command -v docker &> /dev/null 
+then
+    echo "docker is not installed"
+    exit 0 # nothing to stop
+fi
+
 docker kill $(docker ps -q)
 docker image prune -af
